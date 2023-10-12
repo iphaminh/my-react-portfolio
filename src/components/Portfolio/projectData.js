a06a42cbcdddf8fb4e821562311ef480cf131723
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-12 17:13:00 -0500

    this is the right correcttion to insert img

--- src/components/Portfolio/projectData.js
+++ src/components/Portfolio/projectData.js
@@ -1,41 +1,54 @@
+import noteTakerImage from '../../assets/img/Note-Taker.jpg';
+import passwordImage from '../../assets/img/Password.jpg';
+import powerGridImage from '../../assets/img/PowerGrid.jpg';
+import mealPlanImage from '../../assets/img/meal-plan.jpg';
+import quizImage from '../../assets/img/quiz.jpg';
+import simpleTaskImage from '../../assets/img/simple-task.jpg';
+import weatherForecastImage from '../../assets/img/weather-forecast.jpg';
+
 const projectData = [
     {
-      name: 'Quiz',
-      image: '../../assets/img/quiz.jpg',
-      githubLink: 'https://github.com/iphaminh/code-quiz-challenge',
-      liveLink: 'https://iphaminh.github.io/code-quiz-challenge/',
-    },
-    {
-      name: 'Password Generator',
-      image: '../../assets/img/Password.jpg',
-      githubLink: 'https://github.com/iphaminh/random-password-generator',
-      liveLink: 'https://iphaminh.github.io/random-password-generator/',
-    },
-    {
-      name: 'Meal Planner',
-      image: '../../assets/img/meal-plan.jpg',
-      githubLink: 'https://github.com/iphaminh/Meal-Planner',
-      liveLink: 'https://meal-planner-08232023-07a30f8d4150.herokuapp.com/',
-    },
-    {
-      name: 'Power Grid',
-      image: '../../assets/img/PowerGrid.jpg',
-      githubLink: 'https://github.com/larsly/PowerGrid',
-      liveLink: 'https://larsly.github.io/PowerGrid/index.html',
-    },
-    {
       name: 'Note Taker',
-      image: '../../assets/img/Note-Taker.jpg',
+      image: noteTakerImage,
       githubLink: 'https://github.com/iphaminh/Note-Taker',
       liveLink: 'http://localhost:3000',
     },
     {
-      name: 'Weather App',
-      image: '../../assets/img/weather-forecast.jpg',
+      name: 'Password Generator',
+      image: passwordImage,
+      githubLink: 'https://github.com/iphaminh/random-password-generator',
+      liveLink: 'https://iphaminh.github.io/random-password-generator/',
+    },
+    {
+      name: 'Power Grid',
+      image: powerGridImage,
+      githubLink: 'https://github.com/larsly/PowerGrid',
+      liveLink: 'https://larsly.github.io/PowerGrid/index.html',
+    },
+    {
+      name: 'Meal Planner',
+      image: mealPlanImage,
+      githubLink: 'https://github.com/iphaminh/Meal-Planner',
+      liveLink: 'https://meal-planner-08232023-07a30f8d4150.herokuapp.com/',
+    },
+    {
+      name: 'Quiz',
+      image: quizImage,
+      githubLink: 'https://github.com/iphaminh/code-quiz-challenge',
+      liveLink: 'https://iphaminh.github.io/code-quiz-challenge/',
+    },
+    {
+      name: 'Simple Task',
+      image: simpleTaskImage,
+      githubLink: 'https://github.com/iphaminh/Simple-Task',
+      liveLink: 'https://iphaminh.github.io/Simple-Task/',
+    },
+    {
+      name: 'Weather Forecast',
+      image: weatherForecastImage,
       githubLink: 'https://github.com/iphaminh/Traveler-Weather-Companion.',
       liveLink: 'https://iphaminh.github.io/Traveler-Weather-Companion./',
     },
 ];
-
 
 export default projectData;
