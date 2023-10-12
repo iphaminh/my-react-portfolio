25b974c3f2e6ab80548ccb6604ca3f481b1a9bbc
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-12 16:58:04 -0500

    fix up path images

--- src/components/Portfolio/projectData.js
+++ src/components/Portfolio/projectData.js
@@ -1,40 +1,40 @@
 const projectData = [
-    {
-      name: 'Quiz',
-      image: '/assets/img/quiz.jpg',
-      githubLink: 'https://github.com/iphaminh/code-quiz-challenge',
-      liveLink: 'https://iphaminh.github.io/code-quiz-challenge/',
-    },
-    {
-      name: 'Password Generator',
-      image: '/assets/img/Password.jpg',
-      githubLink: 'https://github.com/iphaminh/random-password-generator',
-      liveLink: 'https://iphaminh.github.io/random-password-generator/',
-    },
-    {
-      name: 'Meal Planner',
-      image: '/assets/img/meal-plan.jpg',
-      githubLink: 'https://github.com/iphaminh/Meal-Planner',
-      liveLink: 'https://meal-planner-08232023-07a30f8d4150.herokuapp.com/',
-    },
-    {
-      name: 'Power Grid',
-      image: '/assets/img/PowerGrid.jpg',
-      githubLink: 'https://github.com/larsly/PowerGrid',
-      liveLink: 'https://larsly.github.io/PowerGrid/index.html',
-    },
-    {
-      name: 'Note Taker',
-      image: '/assets/img/Note-Taker.jpg',
-      githubLink: 'https://github.com/iphaminh/Note-Taker',
-      liveLink: 'http://localhost:3000',
-    },
-    {
-      name: 'Weather App',
-      image: '/assets/img/weather-forecast.jpg',
-      githubLink: 'https://github.com/iphaminh/Traveler-Weather-Companion.',
-      liveLink: 'https://iphaminh.github.io/Traveler-Weather-Companion./',
-    },
+  {
+    name: 'Quiz',
+    image: '/assets/img/quiz.jpg',
+    githubLink: 'https://github.com/iphaminh/code-quiz-challenge',
+    liveLink: 'https://iphaminh.github.io/code-quiz-challenge/',
+  },
+  {
+    name: 'Password Generator',
+    image: '/assets/img/Password.jpg',
+    githubLink: 'https://github.com/iphaminh/random-password-generator',
+    liveLink: 'https://iphaminh.github.io/random-password-generator/',
+  },
+  {
+    name: 'Meal Planner',
+    image: '/assets/img/meal-plan.jpg',
+    githubLink: 'https://github.com/iphaminh/Meal-Planner',
+    liveLink: 'https://meal-planner-08232023-07a30f8d4150.herokuapp.com/',
+  },
+  {
+    name: 'Power Grid',
+    image: '/assets/img/PowerGrid.jpg',
+    githubLink: 'https://github.com/larsly/PowerGrid',
+    liveLink: 'https://larsly.github.io/PowerGrid/index.html',
+  },
+  {
+    name: 'Note Taker',
+    image: '/assets/img/Note-Taker.jpg',
+    githubLink: 'https://github.com/iphaminh/Note-Taker',
+    liveLink: 'http://localhost:3000',
+  },
+  {
+    name: 'Weather App',
+    image: '/assets/img/weather-forecast.jpg',
+    githubLink: 'https://github.com/iphaminh/Traveler-Weather-Companion.',
+    liveLink: 'https://iphaminh.github.io/Traveler-Weather-Companion./',
+  },
 ];
-  
+
 export default projectData;
