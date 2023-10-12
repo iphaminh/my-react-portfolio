11619e392908d8b44e7434f0d4be378f0059b794
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-12 16:35:15 -0500

    adding iamges and create projectData

---
+++ src/projectData.js
@@ -0,0 +1,46 @@
+const projectData = [
+    {
+      name: 'Quiz',
+      image: '',
+      githubLink: 'https://github.com/iphaminh/code-quiz-challenge',
+      liveLink: 'https://iphaminh.github.io/code-quiz-challenge/',
+    },
+   
+    {
+        name: 'Password Generator',
+        image: '/path-to-image1.jpg',
+        githubLink: 'https://github.com/iphaminh/random-password-generator',
+        liveLink: 'https://iphaminh.github.io/random-password-generator/',
+      },
+
+      {
+        name: 'Meal Planner',
+        image: '/path-to-image1.jpg',
+        githubLink: 'https://github.com/iphaminh/Meal-Planner',
+        liveLink: 'https://meal-planner-08232023-07a30f8d4150.herokuapp.com/',
+      },
+
+      {
+        name: 'Power Grid',
+        image: '/path-to-image1.jpg',
+        githubLink: 'https://github.com/larsly/PowerGrid',
+        liveLink: 'https://larsly.github.io/PowerGrid/index.html',
+      },
+
+      {
+        name: 'Note Taker',
+        image: '/path-to-image1.jpg',
+        githubLink: 'https://github.com/iphaminh/Note-Taker',
+        liveLink: 'http://localhost:3000',
+      },
+
+      {
+        name: 'Weather App',
+        image: '/path-to-image1.jpg',
+        githubLink: 'https://github.com/iphaminh/Traveler-Weather-Companion.',
+        liveLink: 'https://iphaminh.github.io/Traveler-Weather-Companion./',
+      },
+  ];
+  
+  export default projectData;
+  
